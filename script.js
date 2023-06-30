c6cb80fe928c9bcedd1132cab25ee5461e2c805a
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-06-30 17:43:52 +0200

    style de error numberr

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-// const navBut = document.querySelectorAll('.nav-but').forEach((item)=>{
-//     item.addEventListener('click',()=>{
+const email = document.getElementById('iemail');
+const text = document.getElementById('itext');
+const number = document.getElementById('inumber');
+const enviar = document.getElementById('enviar').addEventListener('click',(e)=>{
+    e.preventDefault();
+    if(text.value.length <= 3 || text.value.length > 25){
+       text.style.cssText = 'color:red; border:1px solid red; '
+    }else if(number.value.length !== 9 ){
+        number.style.cssText = 'color:red; border:1px solid red; '
+s    }
+})
+    
+
         
-//     })
-// })
-
-
