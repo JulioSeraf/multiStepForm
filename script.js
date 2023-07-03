4c6737dcfa8333cf71d6b1a4e3d63755c6e93375
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-07-03 15:27:25 +0200

    creando display de boton info number

--- script.js
+++ script.js
@@ -6,15 +6,17 @@ const yourPlan = document.querySelector('.yourPlan');
 const addOns = document.querySelector('.addOns');
 const finish = document.querySelector('.finish');
 const confirm = false;
-const butDisplays = document.querySelectorAll('.nav-but')
-// BackgroundColor of button page-------------------------------------------------------
+
+
+// BackgroundColor of formulario page-------------------------------------------------------
+
 const enviar = document.getElementById('enviar').addEventListener('click',(e)=>{
     e.preventDefault();
     if(text.value.length <= 3 || text.value.length > 25){
-       text.style.cssText = 'color:red; border:1px solid red; '
+       text.style.cssText = 'color:red; border:1px solid red; ';
     }else if(number.value.length !== 9 ){
-        text.style.cssText = 'color:black; border:1px solid black; '
-        number.style.cssText = 'color:red; border:1px solid red; '
+        text.style.cssText = 'color:black; border:1px solid black; ';
+        number.style.cssText = 'color:red; border:1px solid red; ';
         document.getElementById('errorNumber').style.display = 'block';
 s   }else{
     number.style.cssText = 'color:black; border:1px solid black; '
@@ -24,21 +26,23 @@ s   }else{
 }
 })
 
+function coloInfoPage(e){
+    const butDisplays = document.querySelectorAll('.nav-but');
+         butDisplays[0].style.backgroundColor = "gray";
+
+    }
+
+// display page bottun next/back--------------------------------------------------------------
+
 const butBack = document.querySelectorAll('.but-back').forEach((item)=>{
     item.addEventListener('click', ()=>{
         if(yourPlan.style.display === 'block'){
+            coloInfoPage(0);
             yourPlan.style.display = 'none';
             infoPersonal.style.display = 'block';
         }
     })
 })
 
-// if(infoPersonal.style.display == 'block'){
-//     butDisplays[0].style.cssText = 'background-color:hsl(206, 94%, 87%); color:gray;';
-// }else if(yourPlan.style.display == 'block'){
-//     butDisplays[1].style.cssText = 'background-color:hsl(206, 94%, 87%); color:gray;';
-// }else if(addOns.style.display == 'block'){
-//     butDisplays[2].style.cssText = 'background-color:hsl(206, 94%, 87%); color:gray;';
-// }else if(finish.style.display == 'block'){
-//     butDisplays[3].style.cssText = 'background-color:hsl(206, 94%, 87%); color:gray;';
-// }
+// info number page Background color----------------------------------------------------------
+
